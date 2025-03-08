f8b908433b238028da7a60248b107628ac2de28e
Ammad Qureshi <ammadq196@gmail.com>
2025-03-09 03:05:39 +0500

    Operators in python

---
+++ main.py
@@ -0,0 +1,122 @@
+"Arithmetic Operator"
+"Addition"
+a=10
+b=20
+print(a+b)
+
+"Subtraction"
+a=10
+b=20
+print(b-a)
+print(a-b)
+
+"Multiplication"
+a=10
+b=20
+print(a*b)
+
+"Division"
+a=5   
+b=2
+print(a/b) #Simple division operator give float value
+print(a//b) #Floor division operator give integer value
+
+"Modules Remainder"
+a=10
+b=3
+print(a%b) #Modules operator give remainder
+
+"Exponent"
+a=2
+b=3
+print(a**b) #Exponent operator give power of number
+
+"Assigment Operator"
+y=10+20
+print(y)
+
+"Addition"
+m=15
+m=m+10
+print(m)
+
+"Subtraction"
+m=15
+m=m-10
+print(m)
+
+"Multiplication"
+m=15
+m=m*10
+print(m)
+
+"Divisiom"
+m=15
+m=m/10
+print(m)
+
+"Modules"
+m=15
+m=m%10
+print(m)
+
+"Exponent"
+m=15
+m=m**2
+print(m)
+
+"floor division"
+m=15
+m=m//10
+print(m)
+
+"comparision operator"
+"== #Equal"
+a=10
+b=20
+print(a==b)
+
+#Not Equal 
+a=10    
+b=30    
+print(a!=b)  #not equal operator give false if both values are same
+
+#Greater than
+a=20
+b=30
+print(a>b) #Greater than operator give false if first value is less than second value
+
+#less than
+a=20
+b=30
+print(a<b) #less than operator give false if first value is greater than second value
+
+#greater than equal to
+a=20
+b=10
+print(a>=b) #greater than equal to operator give true if first value is greater than second value and both values are equal so it give true.
+
+#less than equal to
+a=30
+b=20
+print(a<=b) #less than equal to operator give true if first value is less than second value and both values are equal so it give true.
+
+"Logical operator"
+#and operator
+a=10
+b=20
+c=20
+print(a<b and b==c) #and operator give true if both condition are true.
+
+"or operator"
+a=10
+b=20
+c=20
+print(a>b or b==c) #or operator give true if any one condition is true.
+
+#not operator
+a=10
+b=20
+c=20
+print(not a>b) #not operator give true if condition is false and give false is condition is true.
+
